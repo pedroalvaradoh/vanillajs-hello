1e5af5441ce40e8ff779953d0ce477688dd8749e
pedroalvaradoh <129191234+pedroalvaradoh@users.noreply.github.com>
2023-04-22 20:31:00 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function onLoad() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let whoEX = Math.floor(Math.random() * who.length);
+    let actionEX = Math.floor(Math.random() * action.length);
+    let whatEX = Math.floor(Math.random() * what.length);
+    let whenEX = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoEX] +
+      " " +
+      action[actionEX] +
+      " " +
+      what[whatEX] +
+      " " +
+      when[whenEX]
+    );
+  }
+  let button = document.getElementById("btn");
+  button.addEventListener("click", function() {
+    const excusa = onLoad();
+
+    document.querySelector("#excusee").innerHTML = excusa;
+  });
 };
